32c7e01600405ba93f2d6bb0cd0c05dd006a289a
[Jacob Kouns] <[jacob.kouns01@gmail.com]>
2023-03-23 20:35:34 -0500

    adding new style to stylesheet

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,31 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'views/landingpage.handlebars',
-    'views/login.handlebars',
-    'views/register.handlebars',
-   'public/css/style.css'
-  ],
-  
   theme: {
     colors: {
       'white': '#f5f5f5',
       'black': '#171717',
-      'cream': '#fef3c7'
+      'cream': '#fef3c7',
+      'maroon': '#9f1239'
     },
-    fontFamily:{
+    fontFamily: {
       sans: ['ui-serif, Georgia, Cambria, "Times New Roman", Times, serif;'],
       mono: ['ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'],
     },
-    extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius:{
-        'full': "full"
-      },
-    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32',
+      '6': '50px',
+      '7': '64px',
+      '8': '80px'
+
+    }
   },
-  plugins: [],
 }
+
