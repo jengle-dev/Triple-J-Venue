1544513952557a0ffa586ed85617ea6417e54d85
Jacob Boyce <jboyce313@gmail.com>
2023-03-30 18:56:34 -0500

    got rid of duplicate code

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 require("dotenv").config();
 
-// const path = require("path");
-
-// const express = require("express");
-// const session = require("express-session");
-// const exphbs = require("express-handlebars");
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
